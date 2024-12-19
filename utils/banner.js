9475f887f2711b3700e0232838eb19a4ad947897
CryptonodeHindi <cryptonodehindi@gmail.com>
2024-12-19 17:33:10 +0530

    Create banner.js

---
+++ utils/banner.js
@@ -0,0 +1,12 @@
+const banner = `\n
+CCCCC   RRRRR   Y   Y  PPPP   TTTTT  OOO      N   N   OOO   DDDD  EEEEE      H   H  III  N   N  DDDD   III
+C       R   R    Y Y   P  P     T   O   O     NN  N  O   O  D   D E          H   H   I   NN  N  D   D   I 
+C       RRRRR     Y    PPPP     T   O   O     N N N  O   O  D   D EEEE       HHHHH   I   N N N  D   D   I 
+C       R   R     Y    P        T   O   O     N  NN  O   O  D   D E          H   H   I   N  NN  D   D   I 
+ CCCCC  R    R    Y    P        T    OOO      N   N   OOO   DDDD  EEEEE      H   H  III  N   N  DDDD   III
+
+                 Running testnet Plaza Finance Bot - Made by Rajesh
+                       https://linktr.ee/cryptonodehindi
+\n`;
+
+export default banner;
